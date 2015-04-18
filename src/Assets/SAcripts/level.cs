ef34d41ed258b357c64b9c1f2cc7313c8f13666c
superMsp10 <pandeymahan34@gmail.com>
2015-04-18 17:47:37 -0400

    level Generation

--- src/Assets/SAcripts/level.cs
+++ src/Assets/SAcripts/level.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class level : MonoBehaviour {
+public class level : MonoBehaviour
+{
+	public bool turnR = true;
+	public int offSet;
+	public Transform startPos;
+	public int levelsG;
+	public GameObject lev;
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		generateLevels (5);
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void generateLevels (int n)
+	{
+
+		for (int i =0; i<n; i++) {
+			Vector3 pos = startPos.position;
+			pos.y += (levelsG * offSet); 
+			GameObject g = (GameObject)Instantiate (lev, pos, Quaternion.identity);
+			if (turnR) {
+				g.transform.localScale = new Vector3 (-1, g.transform.localScale.y, g.transform.localScale.z);
+				turnR = false;
+			} else {
+				g.transform.localScale = new Vector3 (1, g.transform.localScale.y, g.transform.localScale.z);
+				turnR = true;
+			}
+
+			g.transform.parent = gameObject.transform;
+			levelsG++;
+
+		}
 	}
 }

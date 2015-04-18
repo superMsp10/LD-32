8ca4ba39d797375a940b80cecd871335ab7adf4a
superMsp10 <pandeymahan34@gmail.com>
2015-04-18 14:07:11 -0400

    playerMiovement

--- src/Assets/SAcripts/Player.cs
+++ src/Assets/SAcripts/Player.cs
@@ -3,6 +3,44 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+	public float moveSpeed;
+	public Transform GroundCheck;
+	public LayerMask whatGround;
+	public bool grounded = false;
+	public int jumpAmount;
 
+
+
+	void FixedUpdate ()
+	{
+		checkG ();
+		float move = Input.GetAxis ("Horizontal");
+		moveX (move);
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, rigidbody2D.velocity.y + jumpAmount);
+
+		}
+
+	}
+	public  void moveX (float moveX)
+	{
+		float xMove = (moveX * 1);
+		
+		
+	
+		if (Mathf.Abs (rigidbody2D.velocity.x) < moveSpeed)
+			rigidbody2D.velocity = new Vector2 (xMove + rigidbody2D.velocity.x, rigidbody2D.velocity.y);
+		
+	}
+
+	void checkG ()
+	{
+		if (Physics2D.Linecast (transform.position, GroundCheck.position, whatGround)) {
+			grounded = true;
+		} else {
+			grounded = false;
+		}
+
+	}
 
 }

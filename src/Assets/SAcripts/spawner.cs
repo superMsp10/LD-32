3157a2ecc2f3f31aefd9c667b78cae2df3340e1e
superMsp10 <pandeymahan34@gmail.com>
2015-04-18 22:36:57 -0400

    ChickenSpawners

---
+++ src/Assets/SAcripts/spawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour
+{
+
+	public GameObject[] spawn;
+	public int amount;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		for (int i = 0; i <Random.Range(1,amount); i++) {
+			Invoke ("Ins", Random.Range (0f, 2f));
+		}
+		Destroy (gameObject, 3f);
+	}
+
+	void Ins ()
+	{
+		Debug.Log ("hi");
+
+		Instantiate (spawn [Random.Range (0, spawn.Length)], transform.position, Quaternion.identity);
+
+	}
+
+}
